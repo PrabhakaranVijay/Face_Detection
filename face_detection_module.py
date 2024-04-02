fa3d97a6543ac3c976da3fc2711ee519eccc10e9
PrabhakaranVijay <prabhakaran.vijay.17@gmail.com>
2024-04-02 23:16:31 +0530

    added fps reading

--- face_detection_module.py
+++ face_detection_module.py
@@ -2,19 +2,29 @@ import cv2 as cv
 import mediapipe as mp
 import time
 
-video_path = '/Users/prabhakaranvijay/Machine Learning/OpenCV/videos/1.mov'
+# path of the video file 
+# (or give device index '0' for webCam)
+video_path = 0
 
 video = cv.VideoCapture(video_path)
 
-
+pTime = 0
 
 while True:
     isTrue, frame = video.read()
 
+    # print frame pre seconds FPS
+    # to find speed of capture
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv.putText(frame, f'fps: {int(fps)}',(10,50),cv.FONT_HERSHEY_PLAIN, 2, (0,0,255),2)
+
     cv.imshow('video',frame)
 
+    # press 'd' to exit from the loop 
     if cv.waitKey(1) & 0xFF==ord('d'):
         break
-
+ 
 video.release()
 cv.destroyAllWindows()
